1479f041bd08c3716072b7baf1bbcfbeba9a8463
Oliver Frisvoll <ocfris97@gmail.com>
2023-01-18 11:15:51 +0100

    Changed out datagrid combines the nested list into a dataframe

--- R/datagrid.R
+++ R/datagrid.R
@@ -87,36 +87,43 @@ get_datagrid <- function(instrument, fields, ...) {
 
     }
 
+    results$responses[[1]]$headers[[1]]
+
     data <- results$responses[[1]]$data
+    column_names <- purrr::map_chr(results$responses[[1]]$headers[[1]], ~.$displayName)
 
-    null_results <- purrr::imap_int(data, ~ifelse(!is.null(.x[[2]]), NA, .y))
+    data_df <- purrr::map_dfr(data, ~as.data.frame(purrr::map(., \(x) ifelse(is.null(x), NA, as.character(x))), col.names = column_names))
 
-    null_results <- null_results[!is.na(null_results)]
-    if (length(null_results > 0)) {
+    return(data_df)
 
-        data <- data[-null_results]
 
-    }
-
-    if (length(data) < 0) {
-
-        # TODO: Create test
-        cli::cli_warn(c(
-          "No Results",
-          "x" = "Your query with instruments: {instument[1]}... for fields: {fields[1]}... did not return anything",
-          "i" = "Maybe check the spelling?"
-        ))
-
-    } else {
-
-        column_names <- purrr::map_chr(results$responses[[1]]$headers[[1]], ~.$displayName)
-
-        loop <- function(data) {
-            data <- purrr::map(data, ~ifelse(.x == "", NA, .x))
-            as.data.frame(data, col.names = column_names)
-        }
-
-        purrr::map_dfr(data, loop)
-
-    }
+    # null_results <- purrr::imap_int(data, ~ifelse(!is.null(.x[[2]]), NA, .y))
+    # null_results <- null_results[!is.na(null_results)]
+    # if (length(null_results > 0)) {
+    #
+    #     data <- data[-null_results]
+    #
+    # }
+    #
+    # if (length(data) < 0) {
+    #
+    #     # TODO: Create test
+    #     cli::cli_warn(c(
+    #       "No Results",
+    #       "x" = "Your query with instruments: {instument[1]}... for fields: {fields[1]}... did not return anything",
+    #       "i" = "Maybe check the spelling?"
+    #     ))
+    #
+    # } else {
+    #
+    #     column_names <- purrr::map_chr(results$responses[[1]]$headers[[1]], ~.$displayName)
+    #
+    #     loop <- function(data) {
+    #         data <- purrr::map(data, ~ifelse(.x == "", NA, .x))
+    #         as.data.frame(data, col.names = column_names)
+    #     }
+    #
+    #     purrr::map_dfr(data, loop)
+    #
+    # }
 }
