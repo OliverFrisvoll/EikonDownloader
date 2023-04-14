711a23ccd61d5bc81003fb930b73b63251f47a95
Oliver Frisvoll <ocfris97@gmail.com>
2023-04-14 02:24:56 +0200

    Added tests, have yet to adjust them to fit with RUST, but we're getting there.

--- tests/testthat/test-datagrid.R
+++ tests/testthat/test-datagrid.R
@@ -8,6 +8,7 @@ test_that("get_datagrid(), does not accept non char values", {
 test_that("get_datagrid(), returns values for a correct Instrument and field", {
     skip_on_cran()
     skip_on_ci()
+
     ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
 
     RIC_TSLA_MSFT <- data.frame(
@@ -18,7 +19,9 @@ test_that("get_datagrid(), returns values for a correct Instrument and field", {
     expect_equal(get_datagrid(c('88160R101', '594918104'), 'TR.RICCode'), RIC_TSLA_MSFT)
 
     rm(RIC_TSLA_MSFT)
-    ek_set_APIKEY(NULL)
+
+    # Resetting to load time va
+    .onLoad()
 })
 
 
@@ -28,14 +31,16 @@ test_that("get_datagrid(), returns values for when one Instrument is faulty and 
     ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
 
     RIC_TSLA <- data.frame(
-      Instrument = "88160R101",
-      RIC.Code = "TSLA.O"
+      Instrument = c('88160R101', '5949182324104'),
+      RIC.Code = c("TSLA.O", NA)
     )
 
     expect_equal(get_datagrid(c('88160R101', '5949182324104'), 'TR.RICCode'), RIC_TSLA)
 
     rm(RIC_TSLA)
-    ek_set_APIKEY(NULL)
+
+    # Resetting to load time va
+    .onLoad()
 })
 
 
@@ -45,8 +50,32 @@ test_that("get_datagrid(), if supplied a faulty field, returns an error", {
     ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
 
     expect_error(get_datagrid(c('88160R101', '594918104'), 'TR.sds'), "No Results")
-    ek_set_APIKEY(NULL)
+
+    # Resetting to load time va
+    .onLoad()
 })
+
+
+test_that("Testing edge case of field not existing in one RIC", {
+    skip_on_cran()
+    skip_on_ci()
+    ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
+
+    expect_error(
+      get_datagrid(
+        instrument = '4297589374',
+        fields = c('TR.CLOSE', 'TR.CLOSE.DATE'),
+        SDate = "2020-01-01",
+        EDate = "2023-01-01",
+      ),
+      "No Results"
+    )
+
+
+    # Resetting to load time va
+    .onLoad()
+})
+
 
 test_that("get_datagrid(), returns multiple rows for multiple fields", {
     skip_on_cran()
@@ -55,14 +84,16 @@ test_that("get_datagrid(), returns multiple rows for multiple fields", {
 
     LOT_IPO <- data.frame(
       Instrument = c("AAPL.O", "TSLA.O"),
-      CF_LOTSIZE = c(100, 100),
+      ISIN = c("US0378331005", "US88160R1014"),
       IPO.Date = c("1980-12-12", "2010-06-09")
     )
 
-    expect_equal(get_datagrid(c('AAPL.O', 'TSLA.O'), c('CF_LOTSIZE', 'TR.IPODate')), LOT_IPO)
+    expect_equal(get_datagrid(c('AAPL.O', 'TSLA.O'), c('TR.ISIN', 'TR.IPODate')), LOT_IPO)
 
     rm(LOT_IPO)
-    ek_set_APIKEY(NULL)
+
+    # Resetting to load time va
+    .onLoad()
 })
 
 test_that("get_datagrid(), accepts keyword arguments", {
@@ -70,36 +101,41 @@ test_that("get_datagrid(), accepts keyword arguments", {
     skip_on_ci()
     ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
 
-    ESG <- data.frame(
-      Instrument = c("MSFT.O", "IBM", "TSLA.O", "AAPL.O", "NFLX.O"),
-      ESG.Score = c(92, 72, 63, 77, 29)
+    test <- get_datagrid(
+      instrument = c("MSFT.O", "IBM", "TSLA.O", "AAPL.O", "NFLX.O"),
+      fields = "TR.TRESGScore",
+      SDate = "2019-01-01",
+      EDate = "2021-01-01"
     )
 
-    expect_equal(get_datagrid(c("MSFT.O", "IBM", "TSLA.O", "AAPL.O", "NFLX.O"), "TR.TRESGScore", SDate =
-      "2021-07-07"), ESG, tolerance = 0.5)
+    expect_equal(nrow(test), 10)
+    expect_equal(ncol(test), 2)
+    expect_equal(names(test), c("Instrument", "ESG.Score"))
 
-    rm(ESG)
-    ek_set_APIKEY(NULL)
-})
+    rm(test)
 
-test_that("get_datgrid(), check is empty results somewhere is accepted", {
-    skip_on_cran()
-    skip_on_ci()
-    ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
-
-    df <- get_datagrid(
-      instrument = c('USDSB3L1Y=', "USDSB3L2Y="),
-      fields = c('TR.BIDPRICE', 'TR.ASKPRICE'),
-      SDate = "2010-01-01",
-      EDate = "2010-03-01",
-      Frq = "D"
-    )
-    expect_true(ncol(df) == 3)
-    expect_true(nrow(df) == 81)
-
-    ek_set_APIKEY(NULL)
+    # Resetting to load time va
+    .onLoad()
 })
 
 
-# Makes sure the API_KEY is reset
-ek_set_APIKEY(NULL)
+# test_that("get_datgrid(), check is empty results somewhere is accepted", {
+#     skip_on_cran()
+#     skip_on_ci()
+#     ek_set_APIKEY('f63dab2c283546a187cd6c59894749a2228ce486')
+#
+#     testgd <<- get_datagrid(
+#       instrument = c('USDSB3L1Y=', "USDSB3L2Y="),
+#       fields = c('TR.BIDPRICE', 'TR.ASKPRICE', 'TR.CLOSEPRICE'),
+#       SDate = "2010-01-01",
+#       EDate = "2010-03-01",
+#       Frq = "D"
+#     )
+#
+#     expect_equal(ncol(df), 4)
+#     expect_equal(nrow(df), 81)
+#
+#
+#     # Resetting to load time va
+#     .onLoad()
+# })
